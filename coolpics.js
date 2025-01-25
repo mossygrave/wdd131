fe4277816b47975b153160988c317261d62e809c
Morgan McKay <168682084+mossygrave@users.noreply.github.com>
2025-01-25 16:51:57 -0700

    Delete coolpics.js

--- coolpics.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to toggle navigation menu
-document.addEventListener("DOMContentLoaded", () => {
-    const menuButton = document.querySelector("header button");
-    const navMenu = document.querySelector("header nav ul");
-
-    menuButton.addEventListener("click", () => {
-        navMenu.classList.toggle("show");
-    });
-});
