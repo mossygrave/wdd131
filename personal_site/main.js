4a64635404e175c343923d550173a3f76ef4c942
Morgan McKay <mckayml922@gmail.com>
2025-04-08 09:10:33 -0600

    javascript and other tweaks

---
+++ personal_site/main.js
@@ -0,0 +1,59 @@
+import { services } from "./services.mjs";
+
+function getRandomListEntry(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
+function serviceTemplate(service) {
+    return `
+    <figure class="service">
+        <img src="${service.image}" alt="image of ${service.name}" />
+        <figcaption>
+            <ul class="service__tags">
+                ${tagsTemplate(service.tags)}
+            </ul>
+            <h2><a href="#">${service.name}</a></h2>
+            ${ratingTemplate(service.rating)}
+            <p class="service__description">${service.description}</p>
+        </figcaption>
+    </figure>`;
+}
+
+function tagsTemplate(tags) {
+    return tags.map(tag => `<li>${tag}</li>`).join('');
+}
+
+function renderServices(serviceList) {
+    const mainContainer = document.querySelector("main");
+
+    const servicesHTML = serviceList.map(service => serviceTemplate(service)).join('');
+    mainContainer.innerHTML = `<div class="services-container">${servicesHTML}</div>`;
+}
+
+
+function init() {
+    const service = getRandomListEntry(services);
+    renderservices([service]);
+}
+init();
+
+function filterServices(query) {
+    return services.filter(service => {
+        const lowerQuery = query.toLowerCase();
+        return (
+            service.name.toLowerCase().includes(lowerQuery) ||
+            service.description.toLowerCase().includes(lowerQuery) ||
+            service.tags.some(tag => tag.toLowerCase().includes(lowerQuery)) ||
+            service.serviceIngredient.some(ingredient => ingredient.toLowerCase().includes(lowerQuery))
+        );
+    }).sort((a, b) => a.name.localeCompare(b.name));
+}
+
+const searchForm = document.querySelector(".search-form");
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const query = document.querySelector("#search").value;
+    const filteredservices = filterservices(query);
+    renderservices(filteredservices);
+});
---
+++ personal_site/services.mjs
@@ -0,0 +1,21 @@
+export const services = [
+    {
+        name: 'Lash Lift',
+        image: '',
+        description: '',
+        price: ''
+    },
+    {
+        name: 'Brow Tint',
+        image: '',
+        description: '',
+        price: ''
+    },
+    {
+        name: 'Makeup',
+        image: '',
+        description: '',
+        price: ''
+    }
+]
+export default services
